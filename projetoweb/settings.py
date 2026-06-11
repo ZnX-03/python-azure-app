1bcae06ab73a36df72630665b34df0323de6c9db
ZnX-03 <andartz03@gmail.com>
2026-06-11 13:14:19 -0300

    Update settings.py

--- projetoweb/settings.py
+++ projetoweb/settings.py
@@ -12,6 +12,9 @@ DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS_ENV = os.environ.get('ALLOWED_HOSTS', '')
 ALLOWED_HOSTS = ALLOWED_HOSTS_ENV.split(',') if ALLOWED_HOSTS_ENV else ['*']
 
+CSRF_TRUSTED_ORIGINS_ENV = os.environ.get('CSRF_TRUSTED_ORIGINS', '')
+CSRF_TRUSTED_ORIGINS = CSRF_TRUSTED_ORIGINS_ENV.split(',') if CSRF_TRUSTED_ORIGINS_ENV else []
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
